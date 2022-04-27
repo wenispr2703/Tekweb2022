595b8d7c1d1cc4b5c3ccbe36febe25210c05fd69
Weni Sepriani <97675489+wenispr2703@users.noreply.github.com>
2022-04-28 06:31:05 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,16 +4,16 @@ Vue.createApp({
       message: "Hi!!",
       header: {
         title: "Weni Sepriani",
-        description:
-          "Be beautiful according to your own version.",
+        desc: "Seorang mahasiswi program studi Sistem Informasi Universitas Ahmad Dahlan angkatan tahun 2020. Masih sangat baru dalam hal coding, masih perlu banyak belajar dan referensi.",
+        description: "Be beautiful according to your own version.",
         social: {
           ig: {
             url: "https://instagram.com/weniseprianiii__",
-            title: "weniseprianiii__",
+            title: " weniseprianiii__ ",
           },
           github: {
             url: "https://github.com/wenispr2703",
-            title: "wenispr2703"
+            title: " wenispr2703"
           }
         },
         imageProfile:
@@ -23,23 +23,23 @@ Vue.createApp({
         {
           title: 'Pengaruh Musik Untuk Kesehatan Mental.',
           description: "Gemar mendengarkan musik dapat memberikan dampak bagi kesehatan.",
-          thumbnail: 'https://i.pinimg.com/564x/dc/f0/a6/dcf0a61507f82e29a7c98358ce0814b1.jpg'
+          thumbnail: 'https://i.pinimg.com/564x/eb/53/b2/eb53b291af241664a3d106f749fdf161.jpg'
         },
         {
           title: 'Pengaruh Musik Untuk Kesehatan Mental.',
           description: "Gemar mendengarkan musik dapat memberikan dampak bagi kesehatan.",
-          thumbnail: 'https://i.pinimg.com/564x/dc/f0/a6/dcf0a61507f82e29a7c98358ce0814b1.jpg'
+          thumbnail: 'https://i.pinimg.com/564x/eb/53/b2/eb53b291af241664a3d106f749fdf161.jpg'
         },
         {
           title: 'Pengaruh Musik Untuk Kesehatan Mental.',
           description: "Gemar mendengarkan musik dapat memberikan dampak bagi kesehatan.",
-          thumbnail: 'https://i.pinimg.com/564x/dc/f0/a6/dcf0a61507f82e29a7c98358ce0814b1.jpg'
+          thumbnail: 'https://i.pinimg.com/564x/eb/53/b2/eb53b291af241664a3d106f749fdf161.jpg'
         }
       ]
     };
   },
   mounted:()=>{    
-    axios.get(" https://raw.githubusercontent.com/wenispr2703/tekweb2022/main/contents/header.json ")
+    axios.get("https://raw.githubusercontent.com/wenispr2703/tekweb2022/main/contents/header.json")
     .then((res)=>{
       console.log(res.data);
     })
