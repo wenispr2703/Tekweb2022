56ff7033e717d9374a7fafb36257dc7a31cf030f
Weni Sepriani <97675489+wenispr2703@users.noreply.github.com>
2022-05-18 17:01:55 +0700

    Update Articles

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 Vue.createApp({
   data() {
     return {
-      message: "Hi!!",
+      massage:"Hi",
       header: {
         title: "Weni Sepriani",
-        desc: "Seorang mahasiswi program studi Sistem Informasi Universitas Ahmad Dahlan angkatan tahun 2020. Masih sangat baru dalam hal coding, masih perlu banyak belajar dan referensi.",
+        desc: "Seorang mahasiswi program studi Sistem Informasi Universitas Ahmad Dahlan angkatan tahun 2020. Masih sangat baru dalam hal coding, masih perlu banyak belajar dan referensi. Tidak harus terburu buru tidak harus sekarang, perlahan tapi pasti karna hal indah butuh waktu. Life is not about taking what you sow, stay a good person because the things you sow maybe can be replaced by someone else. Stay as a human being who has the principle of --stay a good person even if you are not treated well.--",
         description: "Be beautiful according to your own version.",
         social: {
           ig: {
@@ -19,32 +19,61 @@ Vue.createApp({
         imageProfile:
           "./images/profile.png",
       },
-      articles: [
-        {
-          title: 'Pengaruh Musik Untuk Kesehatan Mental.',
-          description: "Gemar mendengarkan musik dapat memberikan dampak bagi kesehatan.",
-          thumbnail: 'https://i.pinimg.com/564x/eb/53/b2/eb53b291af241664a3d106f749fdf161.jpg'
-        },
-        {
-          title: 'Pengaruh Musik Untuk Kesehatan Mental.',
-          description: "Gemar mendengarkan musik dapat memberikan dampak bagi kesehatan.",
-          thumbnail: 'https://i.pinimg.com/564x/eb/53/b2/eb53b291af241664a3d106f749fdf161.jpg'
-        },
-        {
-          title: 'Pengaruh Musik Untuk Kesehatan Mental.',
-          description: "Gemar mendengarkan musik dapat memberikan dampak bagi kesehatan.",
-          thumbnail: 'https://i.pinimg.com/564x/eb/53/b2/eb53b291af241664a3d106f749fdf161.jpg'
-        }
-      ]
+      articles:[],
+      article:null,
     };
   },
-  mounted:()=>{    
-    axios.get("https://raw.githubusercontent.com/wenispr2703/tekweb2022/main/contents/header.json")
-    .then((res)=>{
-      console.log(res.data);
-    })
-    .catch((error)=>{
-      console.log(error);
-    })
+  methods: {
+    getHeaderData() {
+      axios
+        .get(
+          "https://raw.githubusercontent.com/wenispr2703/tekweb2022/main/contents/header.json"
+        )
+        .then((res) => {
+          this.header = res.data;
+          this.getArticles();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    getArticles() {
+      axios
+        .get(
+          "https://raw.githubusercontent.com/wenispr2703/tekweb2022/main/contents/articles.json"
+        )
+        .then((res) => {
+          this.articles = res.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    getMarkdown() {
+      const queryString = window.location.search;
+      const urlParams = new URLSearchParams(queryString);
+      const articles = urlParams.get('article');
+      var converter = new showdown.Converter();
+      console.log(articles);
+      axios
+        .get(
+          src = "https://raw.githubusercontent.com/wenispr2703/tekweb2022/main/contents/" + articles
+        )
+        .then((res) => {
+          var html = converter.makeHtml(res.data);
+          this.article = html;
+          console.log(html);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  },
+
+  beforeMount() {
+    this.getHeaderData(),
+      this.getArticles(),
+      this.getMarkdown()
   }
 }).mount("#app");
+
